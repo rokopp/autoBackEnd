93b8ea287cb5e52a8a3ed03a79b34fc41af9043a
haleit <haleit@ttu.ee>
2020-09-21 14:24:35 +0300

    Spring boot template with some basic dependencies

---
+++ src/main/java/com/example/auto24backend/Auto24backendApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto24backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auto24backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auto24backendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auto24backend/Auto24backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto24backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auto24backendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
