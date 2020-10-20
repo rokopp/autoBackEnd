eceeb5b9e9c4e0798db7b4e9ffde9251ba840088
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-10-20 22:59:15 +0300

    Added test check for double registration

--- src/test/java/com/example/auto24backend/controller/AccountControllerTest.java
+++ src/test/java/com/example/auto24backend/controller/AccountControllerTest.java
@@ -35,4 +35,35 @@ class AccountControllerTest {
         Assert.assertEquals("Wrong email", result.getBody());
     }
 
+    @Test
+    public void testRegisterDoubleAccount() {
+
+        JSONObject body = new JSONObject();
+        body.put("userName", "Bob");
+        body.put("password", "Bob1234");
+        body.put("email", "Bob@Bob.Bob");
+        body.put("phoneNumber", "5628179");
+
+        System.out.println(body);
+
+        JSONObject body2 = new JSONObject();
+        body2.put("userName", "Bob");
+        body2.put("password", "Bob1234");
+        body2.put("email", "Bob@Bob.Bob");
+        body2.put("phoneNumber", "5628179");
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+        HttpEntity<String> request2 = new HttpEntity<>(body2.toString(), headers);
+
+        this.restTemplate.postForEntity("/api/register", request, String.class);
+        ResponseEntity<String> result2 = this.restTemplate.postForEntity("/api/register", request2, String.class);
+
+        Assert.assertEquals(200, result2.getStatusCodeValue());
+        Assert.assertEquals("An account already exists with this name and/or e-mail. Please try again.", result2.getBody());
+    }
+
 }
