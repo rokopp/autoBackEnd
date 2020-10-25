10836634f681cde7954f5703440bafe26ec166ce
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-10-25 13:25:37 +0200

    Added little cheat to CORS error, @CrossOrigin

--- src/main/java/com/example/auto24backend/controller/AdvertisementController.java
+++ src/main/java/com/example/auto24backend/controller/AdvertisementController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@CrossOrigin
 @RequestMapping("api/ads")
 @RestController
 public class AdvertisementController {
