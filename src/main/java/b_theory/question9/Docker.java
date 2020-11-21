7709114fdbbf8b182e81836c5844eba737ebfd3a
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-11-21 19:23:20 +0200

    Q9 partially answered F and G to be answered

--- src/main/java/b_theory/question9/Docker.java
+++ src/main/java/b_theory/question9/Docker.java
@@ -4,24 +4,32 @@ public class Docker {
 
     //todo A
     // What is a server?
-    // Answer:
+    // Answer: A server in practical terms is any computer that is doing something for another computer.
+    // When you visit a web page your computer or phone (the client) is asking another computer (the server) for a page.
+    // It builds it and sends it to you. Technically any device is capable of being a server. For example an iPhone is a
+    // server to the Apple Watch.
 
     //todo B
     // What is the difference between build server and production server?
-    // Answer:
+    // Answer: A production server is configured to restrict access to authorized users and to limit control to
+    // system administrators.
+    //
 
     //todo C
     // What is docker?
-    // Answer:
+    // Answer: Docker is a container technology that utilizes the host OS's kernel while putting the userland
+    // applications into a sandbox. It is more lightweight than a full-fledged virtual machine,
+    // but you're applications need to be compatible with the host OS.
 
     //todo D
     // Name and explain docker container benefits over virtual machine setup (java jar as system process and installed nginx)
-    // 1
-    // 2
+    // 1 Docker is lighter. Containers share operating systems whereas virtual machines are designed to emulate virtual hardware.
+    // 2 Docker uses the Docker engine that resides over one Linux instance rather than the virtual resource intensive machines used by virtual machines.
 
     //todo E
     // Name and explain docker container drawback over virtual machine setup (java jar as system process and installed nginx)
-    // 1
+    // 1 Security. Virtual machines offer more security against OS faults and security breaches than Docker
+    // containers. The model of application separation that Docker uses to optimize resource usage comes at the cost of security.
 
     //todo F
     // Name and describe tools for docker architecture
