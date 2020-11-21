4a1fd084755df8816675883877417d14cecdb500
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-11-21 19:32:41 +0200

    Q9 partially answered F and G to be answered Q10 partially answered F to be answered

--- src/main/java/b_theory/question10/DBaaS.java
+++ src/main/java/b_theory/question10/DBaaS.java
@@ -4,27 +4,38 @@ public class DBaaS {
 
     //todo A
     // What is DBaaS (Database as a service)?
-    // Answer:
+    // Answer: Database as a service (DBaaS) is the process of application owners paying an outside provider that
+    // launches and maintains a cloud database for storage, as opposed to having the application owners control the
+    // database themselves.
 
     //todo B
     // Name 2 examples of DBaaS.
-    // 1
-    // 2
+    // 1 Amazon RDS
+    // 2 Amazon DynamoDB
 
     //todo C
     // Name and explain 2 benefits of using DBaaS
-    // 1
-    // 2
+    // 1 Short-Lived Databases
+    // If you need databases to serve as back ends for continuous integration testing, DBaaS will allow you to rapidly
+    // provision the databases, populate them with test data and then delete them when they are no longer needed.
+    // Having the database provisioned for you makes it far faster and simpler to build the required automation for
+    // the testing environment.
+    // 2 A Microservices Design: DBaaS can be particularly helpful, even in a production environment, if you are using
+    // a microservices design composed of a very large number of databases. Even if you have the internal expertise,
+    // you may not want to invest the time and effort in deploying all those databases. DBaaS can get you
+    // into production significantly faster.
 
     //todo D
     // Name and explain 1 drawback of using DBaaS
-    // 1
+    // 1 IO-Bound Workloads: If your database has IO-bound workloads, you may need very powerful hardware,
+    // including high-end CPUs, terabytes of memory and very fast storage. In this case, you may not find a DBaaS
+    // provider that offers a configuration that will meet your needs.
 
     //todo E
     // What other -aaS do you know?
-    // 1
-    // 2
-    // 3
+    // 1 Saas
+    // 2 Iaas
+    // 3 Paas
 
     //todo F
     // In your opinion which aaS are the most useful and should be integrated first.
