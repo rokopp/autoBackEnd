2941af560e6bc988f9db6585fd95756ea72d1646
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-11-21 18:50:56 +0200

    Q7 answered

--- src/main/java/b_theory/question7/CI.java
+++ src/main/java/b_theory/question7/CI.java
@@ -4,28 +4,29 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Answer: Every change submitted to an application - even to development branches - it’s built and tested automatically and continuously,
 
     //todo B
     // What makes integration continuous?
-    // Answer:
+    // Answer:  For every push to the project, there’s a set of scripts the code is checked against.
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
     // [] compile
-    // [] test
+    // [x] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // 1 Integration bugs are detected early and are easy to track down due to small change sets.
+    // This saves both time and money over the lifespan of a project.
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 Waiting times may occur when multiple developers want to integrate their code around the same time
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 Jenkins
+    // 2 Bamboo
 }
