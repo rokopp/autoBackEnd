5ad2c63d5a0272910ddfe20c79e35e5ca929faab
Hans Leit <Hans.leit@gmail.com>
2021-01-03 13:47:20 +0200

    added material design theory question answers

--- src/main/java/c_theory/question13/MaterialDesign.java
+++ src/main/java/c_theory/question13/MaterialDesign.java
@@ -4,10 +4,13 @@ public class MaterialDesign {
 
     //todo A
     // What is material design?
-    // Answer:
+    // Answer: Material Design is a Google's conceptual design philosophy that outlines
+    // how apps should look and work.
 
     //todo B
     // Name and describe 2 principles of material design (there are more than 2)
-    // 1
-    // 2
+    // 1 Use shadows to convey hierarchy:
+    // with shadows you are conveying the visual hierarchy of elements and layers.
+    // 2 Use bold colors:
+    // bold colors make things look more appealing, they compel users to interact with the design.
 }
