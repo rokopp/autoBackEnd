5f717be84a834a14a50b89b8805b98fed16675f3
Hans Leit <Hans.leit@gmail.com>
2021-01-03 13:29:48 +0200

    Part theory answered

--- src/main/java/c_theory/question12/Security.java
+++ src/main/java/c_theory/question12/Security.java
@@ -4,16 +4,16 @@ public class Security {
 
     //todo A
     // What is authentication?
-    // Answer:
+    // Answer: process of validating identity.
 
     //todo B
     // What is authorization?
-    // Answer:
+    // Answer: Authorization is a security mechanism to determine access levels or user/client privileges.
 
     //todo C
     // For web (HTTP backend) Spring Security is configured as a ... ? Explain it.
     // Think or read how Spring Security checks are injection into your application.
     // Configured as (one word):
-    // Description:
+    // Description: it is configured as which role has access to which requests.
 
 }
